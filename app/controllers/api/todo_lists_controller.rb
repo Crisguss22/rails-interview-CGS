8b5f6f125ad7df5c4e94b8335a601f9de9902166
Cristian Salas <csalas@vairix.com>
2025-02-19 02:01:11 -0300

    Added CRUD for api TodoList
    Added CRUD methods for the TodoListsController, routes for those methods and tests as well

--- app/controllers/api/todo_lists_controller.rb
+++ app/controllers/api/todo_lists_controller.rb
@@ -1,10 +1,52 @@
 module Api
   class TodoListsController < ApplicationController
-    # GET /api/todolists
     def index
       @todo_lists = TodoList.all
 
       respond_to :json
+    end
+
+    def create
+      @todo_list = TodoList.new(name: todo_list_params[:name])
+      if @todo_list.save
+        render json: @todo_list
+      else
+        render json: @todo_list.errors.full_messages.to_json, status: 400
+      end
+    end
+
+    def show
+      @todo_list = TodoList.find_by(id: params[:id])
+      if @todo_list.present?
+        render json: @todo_list
+      else
+        render json: {}, status: 404
+      end
+    end
+
+    def update
+      @todo_list = TodoList.find_by(id: todo_list_params[:id])
+      return render json: {}, status: 404 unless @todo_list.present?
+
+      @todo_list.name = todo_list_params[:name] if todo_list_params[:name]
+      if @todo_list.save
+        render json: @todo_list, status: 200
+      else
+        render json: @todo_list.errors.full_messages.to_json, status: 400
+      end
+    end
+
+    def destroy
+      @todo_list = TodoList.find_by(id: params[:id])
+      status = @todo_list&.destroy ? 200 : 400
+
+      render json: {}, status: 
+    end
+
+    private
+
+    def todo_list_params
+      params.permit(:name, :id)
     end
   end
 end
--- spec/controllers/api/todo_lists_controller_spec.rb
+++ spec/controllers/api/todo_lists_controller_spec.rb
@@ -35,4 +35,78 @@ describe Api::TodoListsController do
       end
     end
   end
+
+  describe 'POST create' do
+    it 'returns success and creates todo_list' do
+      post :create, params: { name: 'RoR Project created' }, format: :json
+
+      aggregate_failures 'includes the id and name' do
+        expect(response.status).to eq(200)
+        expect(TodoList.last.name).to eq('RoR Project created')
+      end
+    end
+  end
+
+  describe 'GET show' do
+    let!(:todo_list) { TodoList.create(name: 'Setup RoR project') }
+
+    it 'returns existing todo_list' do
+      get :show, params: { id: todo_list.id }, format: :json
+
+      response_todo_list = JSON.parse(response.body) 
+
+      aggregate_failures 'includes the id and name' do
+        expect(response_todo_list.keys).to match_array(['id', 'name'])
+        expect(response_todo_list['id']).to eq(todo_list.id)
+        expect(response_todo_list['name']).to eq(todo_list.name)
+      end
+    end
+
+    it 'returns error at non-existing todo_list' do
+      get :show, params: { id: todo_list.id+1 }, format: :json
+
+      expect(response.status).to eq(404)
+    end
+  end
+
+  describe 'PATCH update' do
+    let!(:todo_list) { TodoList.create(name: 'Setup RoR project') }
+
+    it 'returns success and updates todo_list' do
+      patch :update, params: { id: todo_list.id, name: 'RoR Project updated' }, format: :json
+
+      aggregate_failures 'includes the id and name' do
+        expect(response.status).to eq(200)
+        expect(TodoList.find(todo_list.id).reload.name).to eq('RoR Project updated')
+      end
+    end
+
+    it 'returns error at non-existing todo_list' do
+      patch :update, params: { id: todo_list.id+1 }, format: :json
+
+      expect(response.status).to eq(404)
+    end
+  end
+
+  describe 'DELETE' do
+    let!(:todo_list) { TodoList.create(name: 'Setup RoR project') }
+
+    it 'returns success deleting existing todo_list' do
+      delete :destroy, params: { id: todo_list.id }, format: :json
+
+      aggregate_failures 'returns success and destroys todo_list' do
+        expect(response.status).to eq(200)
+        expect(TodoList.exists?(todo_list.id)).to be false
+      end
+    end
+
+    it 'returns error deleting non-existing todo_list' do
+      delete :destroy, params: { id: todo_list.id+1 }, format: :json
+
+      aggregate_failures 'returns error and does not destroy todo_list' do
+        expect(response.status).to eq(400)
+        expect(TodoList.exists?(todo_list.id)).to be true
+      end
+    end
+  end
 end
